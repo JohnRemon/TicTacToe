dadab4d758012836a8131305cbe131f2c24cc6d3
John <john1812008@gmail.com>
2025-02-22 17:34:19 +0200

    Added Disable Board Function and Added the coloring of Boards when winning

--- src/application/Main.java
+++ src/application/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/application/main.fxml"));
         Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("TicTacToe");
         primaryStage.show();
